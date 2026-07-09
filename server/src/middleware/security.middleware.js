9cbd37175aa9ea7d533883c4da689dfed75835c4
abhinay mhatre <abhinaymhatre1234@gmail.com>
2026-07-10 02:53:49 +0530

    added hpp compression and 3mb limit for security

--- server/src/middleware/security.middleware.js
+++ server/src/middleware/security.middleware.js
@@ -1,8 +1,10 @@
 import helmet from "helmet";
 import cors from "cors";
 import env from "../config/env.js";
-import ratelimit from 'express-rate-limit';
-
+import ratelimit from "express-rate-limit";
+import hpp from "hpp";
+import compression from "compression";
+import express from "express";
 
 export default function securityMiddleware(app) {
   app.use(helmet());
@@ -14,10 +16,14 @@ export default function securityMiddleware(app) {
   );
   app.use(
     ratelimit({
-        windowMs:env.RATELIMIT_WINDOWS,
-        limit:env.RATELIMIT,
-        legacyHeaders:true,
-        message:"too many request try again after few minutes",
-    })
-  )
+      windowMs: env.RATELIMIT_WINDOWS,
+      limit: env.RATELIMIT,
+      legacyHeaders: true,
+      message: "too many request try again after few minutes",
+    }),
+  );
+  app.use(hpp());
+  app.use(compression());
+  app.use(express.json({ limit: "3mb" }));
+  app.use(express.urlencoded({ extended: true, limit: "3mb" }));
 }
