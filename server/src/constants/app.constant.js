c48f51dcb2e52517853f365787b70a293a0daa74
abhinay mhatre <abhinaymhatre1234@gmail.com>
2026-07-09 22:27:37 +0530

    user model added

--- server/src/constants/app.constant.js
+++ server/src/constants/app.constant.js
@@ -3,5 +3,10 @@ export const app_config = {
         PORT:3000,
         MONGO_URL:'mongodb://localhost:27017/teamSync',
         NODE_DEV:'development',
+    },
+    roles:{
+       SUPER_ADMIN:'SUPER_ADMIN',
+       ADMIN:'ADMIN',
+       VISITOR:'VISITOR', 
     }
 }
