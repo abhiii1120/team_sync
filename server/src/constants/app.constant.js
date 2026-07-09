ff0aa1a5d1b51472df2018b13c26dfc3d88e8d3d
abhinay mhatre <abhinaymhatre1234@gmail.com>
2026-07-10 02:14:47 +0530

    added morgan logger for development use

--- server/src/constants/app.constant.js
+++ server/src/constants/app.constant.js
@@ -2,7 +2,7 @@ export const app_config = {
     default:{
         PORT:3000,
         MONGO_URL:'mongodb://localhost:27017/teamSync',
-        NODE_DEV:'development',
+        NODE_ENV:'development',
     },
     roles:{
        SUPER_ADMIN:'SUPER_ADMIN',
--- server/src/modules/public/auth/auth.routes.js
+++ server/src/modules/public/auth/auth.routes.js
@@ -1,1 +1,9 @@
 import express from 'express';
+
+let router = express.Router();
+
+router.get('/me' , (req,res) => {
+    res.send('request mil rahi hai')
+})
+
+export default router
