e4238981760e21923c64d1729d78af0a2b9b487e
abhinay mhatre <abhinaymhatre1234@gmail.com>
2026-07-10 00:46:55 +0530

    successfully added errorhandler middleware in app.js

--- server/src/middleware/errorHandler.middleware.js
+++ server/src/middleware/errorHandler.middleware.js
@@ -1,9 +1,9 @@
 import { StatusCodes } from "http-status-codes";
 import { success } from "zod";
 
-const errorHandler = () => {
+const errorHandler = (err,req,res,next) => {
 
-    res.status(err.StatusCode || StatusCodes.INTERNAL_SERVER_ERROR).json({
+    res.status(err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR).json({
         message:err.message || 'Internal server error',
         success:false,
         details:err.details || undefined,
