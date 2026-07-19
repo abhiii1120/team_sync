9fb6480214e6e51076ca55c3cb66789ab858e745
abhinay mhatre <abhinaymhatre1234@gmail.com>
2026-07-19 18:02:56 +0530

    successfully added theme in frontend using redux and localhost

--- client/src/app/layouts/DashboardLayout.jsx
+++ client/src/app/layouts/DashboardLayout.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router";
 
 const DashboardLayout = () => {
+  let {mode} = useSelector((store) => store.theme);
+
+  useEffect(() => {
+    if(mode === 'light'){
+      document.body.classList.add("light");
+    }
+    else{
+      document.body.classList.remove("light");
+    }
+  },[mode]);
   return(
   <div>
     <h1>this is nav bar</h1>
