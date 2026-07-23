40b04314321797519fd4d4c5d9c2c7491e98e250
abhinay mhatre <abhinaymhatre1234@gmail.com>
2026-07-23 23:07:38 +0530

    added tailwindmerge for controlling tailwind classes easily

--- client/src/components/ui/CommonButton.jsx
+++ client/src/components/ui/CommonButton.jsx
@@ -1,11 +1,14 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
-const CommonButton = ({ label, disabled }) => {
+const CommonButton = ({ label, disabled,className }) => {
   return (
     <button
       type="submit"
       disabled={disabled}
-      className="w-full py-2.5 rounded-lg text-sm font-semibold text-white bg-linear-to-r from-indigo-500 to-purple-500 hover:from-indigo-400 hover:to-purple-400 transition disabled:opacity-50  disabled:cursor-not-allowed"
+      className={cn(
+        "w-full py-2.5 rounded-lg text-sm font-semibold text-white bg-linear-to-r from-indigo-500 to-purple-500 hover:from-indigo-400 hover:to-purple-400 transition disabled:opacity-50  disabled:cursor-not-allowed",
+      className)}
     >
       {label}
     </button>
